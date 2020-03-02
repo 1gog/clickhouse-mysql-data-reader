adcb595adbf7f7c15d6c8f835708786d7fdbf291
Мирзуев Анатолий <mirzuev@tutu.tech>
2020-03-02 23:02:58 +0300

    test 1

--- clickhouse_mysql/writer/chwriter.py
+++ clickhouse_mysql/writer/chwriter.py
@@ -78,8 +78,8 @@ class CHWriter(Writer):
                     # we need to convert Decimal value to str value for suitable for table structure
                     if type(row[key]) == Decimal:
                         row[key] = str(row[key])
-                    if type(row[key]) == datetime:
-                        row[key] = str(row[key])
+                    #if type(row[key]) == datetime:
+                    #    row[key] = str(row[key])
                 rows.append(row)
 
         logging.debug('class:%s insert %d row(s)', __class__, len(rows))
