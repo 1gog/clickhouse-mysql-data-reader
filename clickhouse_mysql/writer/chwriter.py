97c5cdbeabe1736aaae7635d470834d513475483
Anatoliy <mirzuev.a.v@gmail.com>
2020-03-10 18:27:27 +0300

    111

--- clickhouse_mysql/writer/chwriter.py
+++ clickhouse_mysql/writer/chwriter.py
@@ -80,7 +80,7 @@ class CHWriter(Writer):
                         row[key] = str(row[key])
                     if type(row[key]) == datetime:
                         row[key] = str(row[key])
-                    print("row key : {}".format(row[key]))
+                    print("row key : {}".format(key))
                     print('insert rows: {}'.format(row[key]))
                 print("new row {}".format(row))
                 rows.append(row)
